8cb14a3a1c1a69a176b32af654f69bddc57b7a24
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-12 14:24:33 -0800

    display the digits of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char ch;
+for (num = 0; num <= 9; num++)
+{
+	putchar(num);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
