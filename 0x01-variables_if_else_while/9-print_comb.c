287594ab60c88c27d7b5038b8779e0fc056f9563
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-12 14:48:37 -0800

    ouput 0 to 9 seperated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+for (num = 48; num < 58; num++)
+{
+	putchar(num);
+	if (num != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+	putchar('\n');
+	return (0);
+}
