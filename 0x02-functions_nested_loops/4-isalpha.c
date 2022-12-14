02dd78a28ec9d4182264e6f23ae4636582f953c1
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-13 23:50:09 -0800

    indicate whether an argument is alpha or otherwise"

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Indicate whether an int is alphabet or otherwise
+ *
+ * Return: 1 if c is letter, 0 if number
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
