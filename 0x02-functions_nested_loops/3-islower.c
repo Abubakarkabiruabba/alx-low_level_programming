70623a3f89802328cac8af80e4579e4ef85aad95
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-13 23:37:29 -0800

    substitute the return value 0 if upper while 1 is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,16 +5,16 @@
  * _islower - prints whether an alphabet is lower or otherwise
  *@c: the argument to be checked
  *
- * Return: 0 if lower, 1 if upper
+ * Return: 1 if lower, 0 if upper
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
