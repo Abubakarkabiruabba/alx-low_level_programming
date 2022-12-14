45362c5cfabd00bfa67004645b1428e604ba57eb
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-14 00:29:42 -0800

    return the absolute digit of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _abs - prints the absolute valie of an integer
+ * @n: argument to be checked
+ *
+ * Return: Actual digit
+ */
+int _abs(int n)
+{
+	if (int n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
