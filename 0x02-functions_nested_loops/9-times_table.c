8f6dfce0a5c49c97f780e41fe5be1f8b2937434a
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-14 12:02:42 -0800

    Print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Return: No return
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			times = i * j;
+			if (times < 10)
+			{
+				_putchar('0' + value);
+			}
+			else
+			{
+				_putchar((value / 10) + '0');
+				_putchar((value % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
