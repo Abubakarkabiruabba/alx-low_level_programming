19acddd3274c13ab350e703ae54974fa1386bd89
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-15 12:57:35 -0800

    Indicate whether an input is upper or laworecase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check whether an input is uppercase or lower
+ * @c: Checked input
+ *
+ * Return: 1 if upper 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (_isupper(c) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This dir will give emphasis on more use of functions and nested loops in c
