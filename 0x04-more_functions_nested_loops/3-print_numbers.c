6294eb9277f37a22cca9fc89c61095cb1569f1ed
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-15 13:39:44 -0800

    print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - display the numbers 1 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 1; i <= 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
