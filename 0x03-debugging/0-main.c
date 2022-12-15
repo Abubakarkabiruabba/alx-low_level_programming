978c82e9f6280ed4e841af8b420536d128476bbc
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-15 00:23:28 -0800

    Copy alx codes and makes the necessary changes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
