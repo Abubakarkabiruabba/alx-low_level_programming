c8008a2f58e634fa54bbca9d6c1af34a882f166b
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-15 00:15:49 -0800

    change the valie of i to 0, and make some indentations to match the alx main sample file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,14 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 23;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
