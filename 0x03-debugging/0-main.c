768594d7274c2a41a4588e66f5011a00e407d24f
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-15 00:03:23 -0800

    testing the function that checks positivity or negativity

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 23;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This dir will concentrate on debugging and testing functions been written in other source files
