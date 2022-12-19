67b814081625fae77a775c11329e87bb6e4147d8
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-19 09:06:45 -0800

    Checks the length of a given string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen- Finds the length of a string.
+ *@s:String pointer to the string whose length is to be found.
+ *Return: returns the length of the string.
+ */
+int _strlen(*s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
