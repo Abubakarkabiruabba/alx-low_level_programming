2fc1d957765803cf403051e30822ffc88b283a5a
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-19 10:04:50 -0800

    Put strings to the standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
