034d0af2c8ad2812ce2805ab091590ddd26629ed
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-22 06:39:13 -0800

    Concetenate 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * *_strcat - concetenate two strings
+ *@*dest: firsr input
+ *@*src: second input
+ *
+ * Return:  dest
+ */
+char *_strcat(char *dest, char src)
+{
+	char *dest_end = dest;
+
+	while (*dest_end != '\0')
+	{
+		dest_end++;
+	}
+	while (*src != '\0')
+	{
+		*dest_end = *src;
+		dest_end++;
+		src++;
+	}
+	*dest_end = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This dir shade more light on pointers, array and strings
