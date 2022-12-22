25aaaa502c036032ebc0ff4d13330936c9d8c52d
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-22 14:12:45 -0800

    The _strncat function is similar to the _strcat function, except that it will use at most n bytes from src; and src does not need to be null-terminated if it contains n or more bytes Return a pointer to the resulting string dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concetenate two strings
+ *@dest: firsr input
+ *@src: second input
+ *@n: inserted integer
+ *
+ * Return:  dest
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	char *dest_end = dest;
+
+	while (*dest_end != '\0')
+	{
+		dest_end++;
+	}
+	int i = 0;
+
+	while (*src != '\0' && i < n)
+	{
+		*dest_end = *src;
+		dest_end++;
+		src++;
+		i++;
+	}
+	*dest_end = '\0';
+	return (dest);
+}
