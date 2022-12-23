3b817c18e21d801d00b82d6a800d40cff145a1c8
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-22 17:17:16 -0800

    101: print an integer number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,29 +6,12 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
-
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		n = -n;
 	}
-	else
-	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
