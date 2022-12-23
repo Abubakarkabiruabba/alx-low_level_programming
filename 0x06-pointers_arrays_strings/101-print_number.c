786aed66cbc0f38b1323ad00a9189d0e58c2d92b
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2022-12-22 16:03:00 -0800

    change putchar to _putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		m = n * -1;
 	}
 	else
@@ -29,6 +29,6 @@ void print_number(int n)
 
 	for (; count >= 1; count /= 10)
 	{
-		putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
