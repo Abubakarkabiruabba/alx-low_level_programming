197766583768b4a212ae221c6d30e4ee0b8ebbc6
Abubakarkabiruabba <abubakarkabiruabba@gmail.com>
2023-01-04 17:32:21 -0800

    funtion printing prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,28 +9,32 @@
  */
 int is_prime_number(int n)
 {
-	int is_prime_number(int n)
+	if (n <= 1)
 	{
-		if (n <= 1)
-		{
-			return (0);
-		}
-		if (n == 2)
-		{
-			return (1);
-		}
-		return (is_prime_number_helper(n, 2));
+		return (0);
 	}
-	int is_prime_number_helper(int n, int i)
+	if (n == 2)
 	{
-		if (i == n)
-		{
-			return (1);
-		}
-		if (n % i == 0)
-		{
-			return (0);
-		}
-		return (is_prime_number_helper(n, i + 1));
+		return (1);
 	}
+	return (is_prime_number_helper(n, 2));
+	}
+/**
+* is_prime_number_helper - recursive helper function for is_prime_number
+* @n: number to check
+* @i: current divisor to check
+*
+* Return: 1 if prime, 0 if not
+*/
+int is_prime_number_helper(int n, int i)
+{
+	if (i > n / 2)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number_helper(n, i + 1));
 }
